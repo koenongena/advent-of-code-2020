95ba0e6d15783159928a1d27d04ea9993ed375ee
koen.ongena <gijoke@gmail.com>
2020-12-05 17:39:54 +0100

    Day 5 - part 1

---
+++ day5.js
@@ -0,0 +1,32 @@
+import {getFilePath, readLines} from "./fetchFile.js";
+
+const splitOnLowerHalfSpecification = (lowerHalfSpecification) => {
+    const split = (s, range) => {
+        if (!s) {
+            return range.min;
+        }
+
+        const x = (range.max - range.min) / 2;
+        if (s.charAt(0) === lowerHalfSpecification) {
+            return split(s.slice(1), {min: range.min, max: range.min + Math.floor(x)});
+        } else {
+            return split(s.slice(1), {min: range.min + Math.ceil(x), max: range.max});
+        }
+    };
+    return split;
+};
+const parseRow = s => splitOnLowerHalfSpecification('F')(s, {min: 0, max: 127});
+const parseColumn = s => splitOnLowerHalfSpecification('L')(s, {min: 0, max: 7});
+
+const parseSeatId = s => {
+    const row = parseRow(s.slice(0, 7));
+    const col = parseColumn(s.slice(7));
+
+    return row * 8 + col;
+};
+
+(async () => {
+    const lines = await readLines(await getFilePath(5));
+    const seatIds = lines.map(parseSeatId);
+    console.log(Math.max(...seatIds));
+})();
