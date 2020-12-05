33c7ed8d6b7b7ac7a09f3a27d1ca420ece2499a6
koen.ongena <gijoke@gmail.com>
2020-12-05 14:01:09 +0100

    Day 1 - star 2

--- day1.js
+++ day1.js
@@ -1,29 +1,44 @@
 import {getFilePath, readLines} from "./fetchFile.js";
-import * as fs from "fs";
-import readline from 'readline';
 
 const filterPossibilities = (sum) => (value, arr) => {
     return arr.filter(a => a + value === sum);
 }
 
+
+const findTuplesWithSum = (sum = 2020) => numbers => {
+    const reducer = ((tuples, current, index, lines) => {
+
+        const matches = filterPossibilities(sum)(current, lines.slice(index + 1));
+        return [...tuples, matches.map(match => ([current, match]))]
+
+    });
+    return numbers
+        .sort()
+        .reduce(reducer, [])
+        .flat()
+        .filter(t => t);
+};
+
+const findTripletsWithSum = (sum) => numbers => numbers.sort()
+    .reduce((acc, currentValue, currentIndex, array) => {
+        const tuples = findTuplesWithSum(sum - currentValue)(numbers.slice(currentIndex + 1));
+        const triplets = tuples.map(t => [currentValue, ...t]);
+        return [...acc, triplets];
+    }, [])
+    .flat()
+    .filter(t => t);
+
 (async () => {
     const filePath = await getFilePath(1);
     const lines = await readLines(filePath);
 
-    const reducer = ((tuples, current, index, lines) => {
-        const matches = filterPossibilities(2020)(current, lines.slice(index + 1));
+    const numbers = lines.map(l => parseInt(l, 10));
 
-        return [...tuples, matches.map(match => ([current, match]))]
-    });
+    const tuples = findTuplesWithSum(2020)(numbers);
+    console.log(tuples.map(t => t[0] * t[1]));
 
-    const tuples = lines.map(l => parseInt(l, 10))
-        .sort()
-        .reduce(reducer, [])
-        .flat()
-        .filter(t => t)
-
-    const multiplications = tuples.map(t => t[0] * t[1]);
-    console.log(multiplications);
+    const triplets = findTripletsWithSum(2020)(numbers);
+    console.log(triplets.map(t => t[0] * t[1] * t[2]));
 })()
 
 
