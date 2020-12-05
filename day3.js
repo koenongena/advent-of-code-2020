014300ec30fe73d02f8ef257ed8a810ad27e44b0
koen.ongena <gijoke@gmail.com>
2020-12-05 15:10:23 +0100

    Day 3 - Part 2

--- day3.js
+++ day3.js
@@ -3,7 +3,7 @@ import {getFilePath, readLines} from "./fetchFile.js";
 const atLocation = (x, y) => ({x: x, y: y});
 
 const moveOnBoard = getSquare => (right, down, currentLocation) => {
-    return getSquare(currentLocation.x + 3, currentLocation.y - 1);
+    return getSquare(currentLocation.x + right, currentLocation.y - down);
 };
 
 function getSquareOnBiomeStableForest(lines) {
@@ -23,28 +23,42 @@ const isTree = (square) => {
     return square.mark === '#';
 }
 
-const inFixedHeightForest = (forest) => (location) =>{
+const inFixedHeightForest = (forest) => (location) => {
     return !outsideOfForest(forest)(location);
 };
 const outsideOfForest = (forest) => location => {
     return location.y * -1 >= forest.length;
 };
 
-(async () => {
-    const forest = await readLines(await getFilePath(3));
-
+const countTreeCollisionsInForest = (forest) => {
     const getSquare = getSquareOnBiomeStableForest(forest);
     const move = moveOnBoard(getSquare);
     const inForest = inFixedHeightForest(forest);
 
-    let location = getSquare(0, 0);
-    let count = 0;
-    do {
-        count += isTree(location);
-        console.log(JSON.stringify(location));
-        location = move(3, 1, location);
-    } while (inForest(location))
+    return (slope) => {
+        let location = getSquare(0, 0);
+        let count = 0;
+        do {
+            count += isTree(location);
+            location = move(slope.right, slope.down, location);
+        } while (inForest(location))
+        console.log("Landed on", count, ' trees with slope', slope);
+        return count;
+    };
+};
 
+(async () => {
+    const forest = await readLines(await getFilePath(3));
+
+    const countTreeCollisions = countTreeCollisionsInForest(forest);
+    const count = countTreeCollisions({right: 3, down: 1});
     console.log(count)
 
+    console.log(
+        countTreeCollisions({right: 1, down: 1}) *
+        countTreeCollisions({right: 3, down: 1}) *
+        countTreeCollisions({right: 5, down: 1}) *
+        countTreeCollisions({right: 7, down: 1}) *
+        countTreeCollisions({right: 1, down: 2})
+    )
 })();
