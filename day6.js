622b28f931d034a6b342128a838ff934f88319df
koen.ongena <gijoke@gmail.com>
2020-12-06 10:40:07 +0100

    Day 6 - part 2

--- day6.js
+++ day6.js
@@ -1,17 +1,20 @@
 import {getFilePath, readLines} from "./fetchFile.js";
 
+const intersection = (array1, array2) => array1.filter(value => (array2 || []).includes(value));
+
+
 (async () => {
     const lines = await readLines(await getFilePath(6));
 
     const groups = lines.reduce((acc, line) => {
-        if (!acc.length || line === '') {
-            acc.push({id: acc.length + 1, answers: []});
+        if (line === '') {
+            return [...acc, {id: acc.length + 2, answers: []}]
         }
 
         const group = acc[acc.length - 1];
         group.answers.push(line);
         return acc;
-    }, []);
+    }, [{id: 1, answers: []}]);
 
     const charSetPerGroup = groups.map(g => {
         return new Set(g.answers
@@ -24,4 +27,14 @@ import {getFilePath, readLines} from "./fetchFile.js";
     }, 0)
 
     console.log(sumOfCounts);
+
+    const part2 = groups.map(group => {
+        return group.answers.reduce((acc, answer) => {
+            return intersection(acc, [...answer]);
+        }, [...'abcdefghijklmnopqrstuvwxyz']);
+    }).reduce((sum, answers) => {
+        return sum + answers.length;
+    }, 0)
+    console.log(part2);
+
 })();
