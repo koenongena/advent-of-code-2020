64707c2a00d938d7afe95a73a78ddc961d917879
koen.ongena <gijoke@gmail.com>
2020-12-06 10:44:28 +0100

    Day 6 - Clean up the mess

--- day6.js
+++ day6.js
@@ -1,40 +1,46 @@
 import {getFilePath, readLines} from "./fetchFile.js";
 
 const intersection = (array1, array2) => array1.filter(value => (array2 || []).includes(value));
+const parseGroups = (acc, line) => {
+    if (line === '') {
+        return [...acc, {id: acc.length + 2, answers: []}]
+    }
+
+    const group = acc[acc.length - 1];
+    group.answers.push(line);
+    return acc;
+};
+
+const combineAnswers = g => {
+    return new Set(g.answers
+        .map(a => [...a]) //extract all chars
+        .flat());
+};
+
+const sumOfSets = (sum, currentValue) => {
+    return sum + currentValue.size
+};
+const sumOfArrays = (sum, currentValue) => {
+    return sum + currentValue.length
+};
+
+const intersectAnswers = group => {
+    return group.answers.reduce((acc, answer) => {
+        return intersection(acc, [...answer]);
+    }, [...'abcdefghijklmnopqrstuvwxyz']);
+};
 
 
 (async () => {
     const lines = await readLines(await getFilePath(6));
+    const groups = lines.reduce(parseGroups, [{id: 1, answers: []}]);
 
-    const groups = lines.reduce((acc, line) => {
-        if (line === '') {
-            return [...acc, {id: acc.length + 2, answers: []}]
-        }
-
-        const group = acc[acc.length - 1];
-        group.answers.push(line);
-        return acc;
-    }, [{id: 1, answers: []}]);
-
-    const charSetPerGroup = groups.map(g => {
-        return new Set(g.answers
-            .map(a => [...a]) //extract all chars
-            .flat());
-    });
-
-    const sumOfCounts = charSetPerGroup.reduce((sum, currentValue) => {
-        return sum + currentValue.size
-    }, 0)
-
+    const sumOfCounts = groups.map(combineAnswers)
+        .reduce(sumOfSets, 0)
     console.log(sumOfCounts);
 
-    const part2 = groups.map(group => {
-        return group.answers.reduce((acc, answer) => {
-            return intersection(acc, [...answer]);
-        }, [...'abcdefghijklmnopqrstuvwxyz']);
-    }).reduce((sum, answers) => {
-        return sum + answers.length;
-    }, 0)
+    const part2 = groups.map(intersectAnswers)
+        .reduce(sumOfArrays, 0)
     console.log(part2);
 
 })();
