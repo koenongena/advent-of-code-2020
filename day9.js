51f70636dc821924b2b38a086bfc4ee36fa37d06
koen.ongena <gijoke@gmail.com>
2020-12-09 10:00:51 +0100

    Day 9 - part 2 - WIP

--- day9.js
+++ day9.js
@@ -1,4 +1,5 @@
 import {readLinesForDay} from "./fetchFile.js";
+import {first, last, sum} from "./utils/arrays.js";
 
 const filter2NumberCombinationWithSum = sum => (arr) => {
     return arr.reduce((acc, v, i) => {
@@ -7,13 +8,40 @@ const filter2NumberCombinationWithSum = sum => (arr) => {
     }, []);
 };
 
+const dropUntilLessThan = s => (arr) => {
+    if (sum(arr) >= s) {
+        return dropUntilLessThan(s)(arr.slice(1));
+    }
+    return arr;
+};
+
+const filterContinuousSetsWithSum = (s) => (arr) => {
+    const cs = arr.reduce((continuousSet, v) => {
+        if (sum(continuousSet) === s && continuousSet.length > 1) {
+            //ignore this value, we've found the set
+            return continuousSet;
+        }
+        const newSum = v + sum(continuousSet);
+        if (newSum <= s) {
+            return [...continuousSet, v];
+        } else {
+            return dropUntilLessThan(s)([...continuousSet, v]);
+        }
+    }, []);
+
+    if (sum(cs) === s) {
+        return cs;
+    }
+    return [];
+};
+
 (async () => {
     const lines = await readLinesForDay(9);
 
     const numbers = lines.map(l => parseInt(l, 10));
     const PREAMBLE_LENGTH = 25;
 
-    const result = numbers.filter((value, index, array) => {
+    const result = first(numbers.filter((value, index, array) => {
         if (index <= PREAMBLE_LENGTH - 1) {
             return false;
         }
@@ -22,6 +50,12 @@ const filter2NumberCombinationWithSum = sum => (arr) => {
         const combinations = filter2NumberCombinationWithSum(value)(preamble)
         return combinations.length === 0;
 
-    })
+    }))
     console.log(result)
+
+    const continuousSet =
+        filterContinuousSetsWithSum(result)(numbers)
+
+    console.log(continuousSet);
+    console.log("Result part 2: ", Math.min(...continuousSet) + Math.max(...continuousSet));
 })();
--- utils/arrays.js
+++ utils/arrays.js
@@ -7,3 +7,6 @@ export const containsAny = (requiredFields) => (arr) => intersection(requiredFie
 export const contains = (s) => (arr) => arr.some(a => a === s);
 
 export const last = (arr) => arr[arr.length - 1];
+export const first = (arr) => (arr || [undefined])[0];
+
+export const sum = (arr) => arr.reduce((acc, v) => acc + v, 0);
