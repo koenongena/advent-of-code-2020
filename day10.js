0d2e110c93aa6cfd1b4d84a7525ea8d775556303
koen.ongena <gijoke@gmail.com>
2020-12-11 08:51:53 +0100

    Day 10 - Part 2. Using tree leaf count

--- day10.js
+++ day10.js
@@ -1,4 +1,31 @@
 import {readLinesForDay} from "./fetchFile.js";
+import {sum} from "./utils/arrays.js";
+
+function createTree(arr) {
+    const cache = new Map();
+    const endpoint = Math.max(...arr);
+
+    const cached = (delegate) => (value) => {
+        if (!cache.has(value)) {
+            const result = delegate(value);
+            cache.set(value, result);
+        }
+        return cache.get(value);
+    }
+
+    const countLeafs = (value) => {
+        if (value === endpoint) {
+            return 1;
+        }
+        const children = arr.filter(a => value < a && a <= value + 3).sort((a, b) => a-b);
+        console.log("Children of %s: %s", value, children);
+        if (children.length === 0) {
+            return 0;
+        }
+        return sum(children.map(child => cached(countLeafs)(child)));
+    };
+    return cached(countLeafs);
+}
 
 (async () => {
     const lines = await readLinesForDay(10);
@@ -19,4 +46,7 @@ import {readLinesForDay} from "./fetchFile.js";
     }, [0, 0, 0, 0]);
 
     console.log(differences[1] * differences[3])
+
+    const countLeafs = createTree(all);
+    console.log(countLeafs(0));
 })();
--- utils/arrays.js
+++ utils/arrays.js
@@ -4,9 +4,11 @@ export const containsAll = (requiredFields) => (arr) => intersection(requiredFie
 
 export const containsAny = (requiredFields) => (arr) => intersection(requiredFields, arr).length > 0;
 
-export const contains = (s) => (arr) => arr.some(a => a === s);
+export const contains = (s) => (arr) => arr.some(a => JSON.stringify(a) === JSON.stringify(s));
 
 export const last = (arr) => arr[arr.length - 1];
 export const first = (arr) => (arr || [undefined])[0];
 
 export const sum = (arr) => arr.reduce((acc, v) => acc + v, 0);
+
+export const range = (min, maxExclusive) => Array.from(new Array(maxExclusive - min).keys());
