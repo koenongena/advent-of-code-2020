481a7c227bb2cee699f52cba3f2d8914096d9502
koen.ongena <gijoke@gmail.com>
2020-12-16 20:48:08 +0100

    Day 16 - Part 1

--- day16.js
+++ day16.js
@@ -12,11 +12,47 @@ const splitOn = (char, arr) => {
 
 const splitAndParse = R.pipe(R.split(','), R.map(parseInteger));
 
+function parseRange(s) {
+    const [min, max] = R.split('-', s);
+    return (value) => {
+        console.log("Checking ", value, ' between ', min, 'and', max)
+        return value >= min && value <= max;
+    }
+}
+
+function parseRule(s) {
+    const ruleName = R.split(':', s)[0];
+    const rules = R.split(':', s)[1];
+    const rangeValidations = rules.split(' or ').map(parseRange);
+    const fullValue = R.anyPass(rangeValidations);
+    return {
+        name: ruleName,
+        isValid: fullValue
+    }
+}
+
+const isInvalid = R.curry((rules, field) => {
+    return R.none(rule => rule.isValid(field), rules);
+});
+
+const notValidForAnyField = (rules) => (fields) => {
+    console.log("Checking ", fields);
+    return R.any(isInvalid(rules), fields);
+};
+const filterInvalidFields = (rules) => (fields) => {
+    return R.filter(isInvalid(rules), fields);
+};
+
 (async () => {
 
     const lines = await readLinesForDay(16);
     const chunks = splitOn('', lines);
-    const rules = chunks[0];
+    const rules = R.map(parseRule, chunks[0]);
     const myTickets = splitAndParse(R.last(chunks[1]));
+
     const nearbyTickets = R.map(splitAndParse, R.tail(chunks[2]));
+
+    const invalidFields = R.map(filterInvalidFields(rules), nearbyTickets);
+    const notEmpty = R.complement(R.isEmpty);
+    console.log(R.sum(R.flatten(R.filter(notEmpty, invalidFields))));
 })();
