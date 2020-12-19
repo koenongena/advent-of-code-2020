d0896fa25c8e3754862c6c715d5fd590537b2397
koen.ongena <gijoke@gmail.com>
2020-12-19 15:09:01 +0100

    Day 17 - part 2 - way too slow. Will refactor after I read about efficient implementations of conways game of life in JS.

--- day17.js
+++ day17.js
@@ -33,25 +33,26 @@ const parseLine = (coordinates, line, y) => {
     const cells = [...line];
     const lineCoordinates = cells.reduce(((acc, cell, x) => {
         return [...acc, {
-            coordinate: {x, y: y === 0 ? 0 : -y, z: 0},
+            coordinate: {x, y: y === 0 ? 0 : -y, z: 0, w: 0},
             active: (cell === '#')
         }]
     }), []);
     return [...coordinates, ...lineCoordinates];
 };
 
-const getNeighboursCoordinates = ({x, y, z}) => {
+const getNeighboursCoordinates = ({x, y, z, w}) => {
     const xy = R.xprod(R.range(x - 1, x + 2), R.range(y - 1, y + 2));
     const xyz = R.map(R.flatten, (R.xprod(xy, R.range(z - 1, z + 2))));
+    const xyzw = R.map(R.flatten, (R.xprod(xyz, R.range(w - 1, w + 2))));
 
-    const center = (xyz => xyz[0] === x && xyz[1] === y && xyz[2] === z);
-    const toCoordinate = (arr) => ({x: arr[0], y: arr[1], z: arr[2]})
-    return R.map(toCoordinate, R.reject(center, xyz));
+    const center = (xyzw => xyzw[0] === x && xyzw[1] === y && xyzw[2] === z && xyzw[3] === w);
+    const toCoordinate = (arr) => ({x: arr[0], y: arr[1], z: arr[2], w: arr[3]})
+    return R.map(toCoordinate, R.reject(center, xyzw));
 }
 
 const formatCoordinate = (coord) => {
-    const {x, y, z} = coord;
-    return `(${x},${y},${z})`
+    const {x, y, z, w} = coord;
+    return `(${x},${y},${z},${w})`
 };
 
 const nextState = R.curry((grid, cube) => {
@@ -99,15 +100,19 @@ const executeCycle = (grid) => {
     const maxZ = maxXYZ("z", grid) + 1;
     const maxY = maxXYZ("y", grid) + 1;
     const maxX = maxXYZ("x", grid) + 1;
+    const maxW = maxXYZ("w", grid) + 1;
 
-    for (let z = minXYZ("z", grid) - 1; z <= maxZ; z++) {
-        for (let y = minXYZ("y", grid) - 1; y <= maxY; y++) {
-            for (let x = minXYZ("x", grid) - 1; x <= maxX; x++) {
-                const cube = {
-                    coordinate: {x, y, z},
-                    active: isActive(grid, {x, y, z})
+
+    for (let w = minXYZ("w", grid) - 1; w <= maxW; w++) {
+        for (let z = minXYZ("z", grid) - 1; z <= maxZ; z++) {
+            for (let y = minXYZ("y", grid) - 1; y <= maxY; y++) {
+                for (let x = minXYZ("x", grid) - 1; x <= maxX; x++) {
+                    const cube = {
+                        coordinate: {x, y, z, w},
+                        active: isActive(grid, {x, y, z, w})
+                    }
+                    newGrid.set(formatCoordinate({x, y, z, w}), nextState(grid, cube));
                 }
-                newGrid.set(formatCoordinate({x, y, z}), nextState(grid, cube));
             }
         }
     }
