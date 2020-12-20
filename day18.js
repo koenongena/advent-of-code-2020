a92654c1b9877eb156e1c6733cdfc0811434369f
koen.ongena <gijoke@gmail.com>
2020-12-20 17:22:35 +0100

    Day 18 - part 1.

--- day18.js
+++ day18.js
@@ -1,10 +1,29 @@
 import R from 'ramda';
 import {parseInteger} from "./utils/numbers.js";
+import {readLinesForDay} from "./fetchFile.js";
 
 const splitOnPlus = R.split('+')
 const splitOnMultiply = R.split('*')
 
+function findEndBracketBracketIndex(s) {
+    let index = 0;
+    let depth = 1;
+    while (depth > 0 && index < s.length) {
+        if (s[index] === ')') {
+            depth--;
+        } else if (s[index] === '(') {
+            depth++;
+        }
+        index++;
+    }
+    return index;
+}
 function takeNumber(s) {
+    if (R.head(s) === '(') {
+        const endIndex = findEndBracketBracketIndex(s.substring(1));
+        const resultInBetweenBrackets = evaluateExpression(s.substring(1, endIndex));
+        return {number: resultInBetweenBrackets, remaining: s.substring(endIndex + 1)};
+    }
     const n = R.head(R.flatten(R.map(splitOnMultiply, splitOnPlus(s))));
     return {number: parseInteger(n.trim()), remaining: s.substring(n.length)};
 }
@@ -31,11 +50,16 @@ function evaluateExpression(s) {
 const removeSpaces = R.replace(/ /g, '');
 
 (async () => {
-    // const lines = await readLinesForDay(18);
+    const lines = await readLinesForDay(18);
 
     const test = "1 + 2 * 3 + 4 * 5 + 6";
-    console.log(takeNumber(removeSpaces(test)));
-    console.log(evaluateExpression(removeSpaces(test)));
-    const test2 = "1 + (2 * 3) + (4 * (5 + 6))";
+    // console.log(takeNumber(removeSpaces(test)));
+    // console.log(evaluateExpression(removeSpaces(test)));
+    const test2 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
     console.log(evaluateExpression(removeSpaces(test2)));
+
+    const part1 = lines.reduce((sum, expression) => {
+        return sum + evaluateExpression(removeSpaces(expression));
+    }, 0)
+    console.log(part1);
 })();
