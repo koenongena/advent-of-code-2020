663667c6368ee2f67d12c91c8fe50491ceaf36f2
koen.ongena <gijoke@gmail.com>
2020-12-21 20:57:51 +0100

    Day 19 - part 1 faster solution

--- day19.js
+++ day19.js
@@ -4,11 +4,11 @@ import {parseInteger} from "./utils/numbers.js";
 
 const mapRule = (s) => {
     const [id, specification] = R.split(": ")(s);
-    return {id: parseInteger(id), specification}
+    return {id: parseInteger(id), specification: R.replace(/"/g, '', specification)}
 }
 
 const findRuleWithId = R.curry((id, rules) => {
-    return R.head(R.filter(R.pipe(R.prop("id"), R.equals(id)), rules));
+    return rules.get(id);
 });
 
 
@@ -20,71 +20,49 @@ function isNaN(char) {
     return Number.isNaN(parseInt(char, 10));
 }
 
-export const inlineSpecification2 = R.curry((rules, specification) => {
-    console.log("Inlining ", specification)
-    if (specification.indexOf('|') >= 0) {
-        return specification.split('|').map(inlineSpecification(rules));
-    }
-    return specification.split(' ').reduce((possibilities, char) => {
-        const id = parseInt(char, 10)
-        if (isNaN(char)) {
-            const replace = R.replace(/"/g, '', char);
-            if (possibilities.length === 0) {
-                return [replace];
-            }
-            return R.xprod(possibilities, [replace]);
-        } else {
-            const inliningSSpecification = findRuleWithId(parseInteger(id))(rules).specification;
-            const inlineSpecification1 = inlineSpecification(rules, inliningSSpecification);
-            if (possibilities.length === 0) {
-                return inlineSpecification1;
-            }
-            return R.xprod(possibilities, inlineSpecification1);
-        }
-    }, []).map(R.join(''));
-});
-
-export const inlineSpecification = R.curry((rules, specification) => {
-    if (R.contains("|", specification)) {
-        return R.flatten(R.map(inlineSpecification(rules), R.split('|', specification)));
+export const matchesRule = (rules, spec) => (s) => {
+    // console.log("Evaluating ", spec);
+    if (s.length === 0  || spec.length === 0) {
+        return s.length === spec.length;
     }
 
-    if (R.contains(' ', specification)) {
-        const individual = specification.split(' ').map(a => inlineSpecification(rules, a));
-        return R.reduce((result, s) => {
-            return R.xprod(result, s).map(arr => arr.join(''));
-        }, [""], individual);
-    }
-    if (isNaN(specification)) {
-        return [R.replace(/"/g, '', specification)];
+    if (R.contains('|', spec)) {
+        const individualSpecs = spec.split('|');
+        return individualSpecs.some(spec => {
+            return matchesRule(rules, spec)(s);
+        });
     }
 
-    const ruleId = parseInteger(specification);
-    const ruleSpecification = findRuleWithId(ruleId, rules).specification;
-    return inlineSpecification(rules, ruleSpecification);
-});
-
-
-function splitToMatchesString(specification) {
-    const removeDoubleQuotes = R.replace(/"/g, '');
-    const removeSpaces = R.replace(/ /g, '');
-    const s = removeSpaces(removeDoubleQuotes(specification));
-    return R.split('|', s);
-}
-
-const inList = (matches) => (s) => {
-    return matches.indexOf(s) > -1;
-}
+    const char = R.split(' ', spec)[0];
+    const dropChar = R.drop(char.length + 1)
+    if (isNaN(char)) {
+        const nextSpec = dropChar(spec);
+        const nextString = R.drop(1, s);
+        return R.head(s) === char && matchesRule(rules, nextSpec)(nextString)
+    } else {
+        const ruleToInline = findRuleWithId(parseInteger(char), rules);
+        const specs = ruleToInline.split(' | ').map(ss => {
+            return R.replace(/ +/g, ' ', ss + " " + dropChar(spec));
+        })
+        return specs.some(spec2 => matchesRule(rules, spec2)(s));
+    }
+};
 
 (async () => {
     const lines = await readLinesForDay(19);
 
     const splitOnNewLines = R.splitWhen(R.equals(''));
     const [ruleSpecifications, input] = splitOnNewLines(lines);
-    const rules = R.map(mapRule, ruleSpecifications);
+    const rules = R.reduce((m, rule) => {
+        const {id, specification } = mapRule(rule)
+        return m.set(id, specification);
+    }, new Map(), ruleSpecifications);
 
-    const rule0Specification = findRuleWithId(0, rules).specification;
-    const validRule0Possibilities = inlineSpecification(rules, rule0Specification);
-    const bla = R.filter(inList(validRule0Possibilities), input)
-    console.log(bla.length)
+    const rule0Specification = findRuleWithId(0, rules);
+    // const validRule0Possibilities = inlineSpecification(rules, rule0Specification);
+    // const bla = R.filter(inList(validRule0Possibilities), input)
+    // console.log(bla.length)
+
+    const part1 = R.filter(matchesRule(rules, rule0Specification), input);
+    console.log(part1.length);
 })();
