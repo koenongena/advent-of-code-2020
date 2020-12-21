db4e02ada861e92bea2f39d575ea8082c3d6ea1a
koen.ongena <gijoke@gmail.com>
2020-12-21 21:02:02 +0100

    Day 19 - part 2

--- day19.js
+++ day19.js
@@ -20,32 +20,34 @@ function isNaN(char) {
     return Number.isNaN(parseInt(char, 10));
 }
 
-export const matchesRule = (rules, spec) => (s) => {
-    // console.log("Evaluating ", spec);
-    if (s.length === 0  || spec.length === 0) {
-        return s.length === spec.length;
-    }
+export const matchesRule = (rules, spec) => {
+    return (s) => {
+        // console.log("Evaluating ", spec);
+        if (s.length === 0 || spec.length === 0) {
+            return s.length === spec.length;
+        }
 
-    if (R.contains('|', spec)) {
-        const individualSpecs = spec.split('|');
-        return individualSpecs.some(spec => {
-            return matchesRule(rules, spec)(s);
-        });
-    }
+        if (R.contains('|', spec)) {
+            const individualSpecs = spec.split('|');
+            return individualSpecs.some(spec => {
+                return matchesRule(rules, spec)(s);
+            });
+        }
 
-    const char = R.split(' ', spec)[0];
-    const dropChar = R.drop(char.length + 1)
-    if (isNaN(char)) {
-        const nextSpec = dropChar(spec);
-        const nextString = R.drop(1, s);
-        return R.head(s) === char && matchesRule(rules, nextSpec)(nextString)
-    } else {
-        const ruleToInline = findRuleWithId(parseInteger(char), rules);
-        const specs = ruleToInline.split(' | ').map(ss => {
-            return R.replace(/ +/g, ' ', ss + " " + dropChar(spec));
-        })
-        return specs.some(spec2 => matchesRule(rules, spec2)(s));
-    }
+        const char = R.split(' ', spec)[0];
+        const dropChar = R.drop(char.length + 1)
+        if (isNaN(char)) {
+            const nextSpec = dropChar(spec);
+            const nextString = R.drop(1, s);
+            return R.head(s) === char && matchesRule(rules, nextSpec)(nextString)
+        } else {
+            const ruleToInline = findRuleWithId(parseInteger(char), rules);
+            const specs = ruleToInline.split(' | ').map(ss => {
+                return R.replace(/ +/g, ' ', ss + " " + dropChar(spec));
+            })
+            return specs.some(spec2 => matchesRule(rules, spec2)(s));
+        }
+    };
 };
 
 (async () => {
@@ -58,11 +60,13 @@ export const matchesRule = (rules, spec) => (s) => {
         return m.set(id, specification);
     }, new Map(), ruleSpecifications);
 
-    const rule0Specification = findRuleWithId(0, rules);
-    // const validRule0Possibilities = inlineSpecification(rules, rule0Specification);
-    // const bla = R.filter(inList(validRule0Possibilities), input)
-    // console.log(bla.length)
-
-    const part1 = R.filter(matchesRule(rules, rule0Specification), input);
+    const part1 = R.filter(matchesRule(rules, rules.get(0)), input);
     console.log(part1.length);
+
+    const adaptedRules = new Map(rules);
+    adaptedRules.set(8, "42 | 42 8");
+    adaptedRules.set(11, "42 31 | 42 11 31");
+
+    const part2 = R.filter(matchesRule(adaptedRules, adaptedRules.get(0)), input);
+    console.log(part2.length);
 })();
