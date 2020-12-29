c088ae4610864ba1d4819ffff752c6c6e0cfb501
koen.ongena <gijoke@gmail.com>
2020-12-29 12:30:02 +0100

    Day 23 - part 2

--- day23.js
+++ day23.js
@@ -16,24 +16,23 @@ const pickup = (amount) => (input) => {
 
     firstNextCup.prev = undefined;
     thirdNextNextCup.next = undefined;
-    return {...input, 'pick up': [firstNextCup, secondNextCup, thirdNextNextCup], cups: cups}
+    input['pick up'] = [firstNextCup, secondNextCup, thirdNextNextCup];
+    return input;
 };
 
 const addDestinationCup = input => {
     const {current, cups} = input;
     const pickedUpCups = input['pick up'];
 
-    const minusOne = (cups) => (l) => l <= 1 ? cups.length - 1: l - 1;
+    const minusOne = (cups) => (l) => l <= 1 ? cups.length - 1 : l - 1;
     const notPickedUp = (pickedUpCups) => {
         const pickedUpIds = R.map(R.prop("id"), pickedUpCups);
         return (newLabel) => newLabel > 0 && !R.includes(newLabel, pickedUpIds);
     }
     const destination = R.until(notPickedUp(pickedUpCups), minusOne(cups), current - 1);
-    return {
-        ...input,
-        destination
-    }
-};
+    input.destination = destination;
+    return input;
+}
 
 const placePickup = (input) => {
     const pickedUpCups = input['pick up'];
@@ -47,11 +46,8 @@ const placePickup = (input) => {
 
 const chooseNewCurrentCup = (input) => {
     const {cups, current} = input;
-
-    return {
-        ...input,
-        current: cups[current].next
-    }
+    input.current = cups[current].next;
+    return input;
 }
 
 const printMoveResult = (startLabel) => (input) => {
@@ -77,7 +73,7 @@ const incrementMove = (input) => {
 }
 
 const createLinkedList = labels => {
-    const max = Math.max(...labels);
+    const max = labels.length;
     const last = R.last(labels);
     const first = R.head(labels);
     const reduceIndexed = R.addIndex(R.reduce);
@@ -99,7 +95,28 @@ const createLinkedList = labels => {
 
     const executeMove = R.pipe(incrementMove, pickupThree, addDestinationCup, placePickup, chooseNewCurrentCup);
 
-    const result = R.until((i) => i.move === 100, executeMove, {cups: cups, move: 0, current: R.head([...input].map(parseInteger))});
+    //part 1
+    const part1 = R.until((i) => i.move === 100, executeMove, {
+        cups: cups,
+        move: 0,
+        current: R.head([...input].map(parseInteger))
+    });
+    printMoveResult(1)(part1);
 
-    printMoveResult(1)(result);
+    //part 2
+    const extraLabels = Array.from({length: 1_000_000 - input.length}, (_, i) => i + 10);
+    const additionalCups = createLinkedList(
+        R.concat([...input].map(parseInteger), extraLabels)
+    );
+
+    const part2 = R.until((i) => i.move === 10_000_000, executeMove, {
+        cups: additionalCups,
+        move: 0,
+        current: R.head([...input].map(parseInteger))
+    });
+    const part2Cups = part2.cups;
+    const next1 = part2Cups[part2Cups[1].next];
+    const next2 = part2Cups[next1.next];
+
+    console.log(next1.id * next2.id);
 })();
